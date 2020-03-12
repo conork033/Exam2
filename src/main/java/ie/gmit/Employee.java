f78724aa6a65c6cc9367a4c6c81bce637c8a015a
CONOR KENNY - STUDENT <G00352227@gmit.ie>
2020-03-12 12:44:44 +0000

    exam

--- src/main/java/ie/gmit/Employee.java
+++ src/main/java/ie/gmit/Employee.java
@@ -1,4 +1,81 @@
 package ie.gmit;
-
+/*
+Conor Kenny
+G003522227
+Exam2 Employee Class
+12/03/20
+*/
 public class Employee {
+
+    private String name;
+    private String phone;
+    private String employeeId;
+    private int Age;
+
+    public Employee(String name, String phone, String employeeId, int age) {
+
+        setName(name);
+        setPhone(phone);
+        setEmployeeId(employeeId);
+        setAge(age);
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    //******************************************
+    public void setName(String name){
+        if(name.length()>3 && name.length()<25){
+            this.name = name;
+        }else{
+
+            throw new IllegalArgumentException("Invalid Name");
+        }
+    }
+    //******************************************
+    public String getPhone() {
+        return phone;
+    }
+    //******************************************
+    public void setPhone(String phone) {
+        if(phone.length() == 8){
+            this.phone = phone;
+
+        }else{
+
+            throw new IllegalArgumentException("Invalid Phone");
+        }
+
+    }
+    //******************************************
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+    //******************************************
+
+    public void setEmployeeId(String employeeId){
+        if(employeeId.length() == 5){
+            this.employeeId = employeeId;
+        }else{
+
+            throw new IllegalArgumentException("Invalid ID");
+        }
+    }
+    //******************************************
+
+    public int getAge() {
+        return Age;
+    }
+    //******************************************
+
+    public void setAge(int age){
+        if(age>16){
+            Age = age;
+        }else{
+            throw new IllegalArgumentException("Invalid Age");
+        }
+    }
 }
--- src/test/java/ie/gmit/EmployeeTest.java
+++ src/test/java/ie/gmit/EmployeeTest.java
@@ -1,4 +1,57 @@
 package ie.gmit;
+/*
+Conor Kenny
+G003522227
+Exam2 EmployeeTest Class
+12/03/20
+*/
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EmployeeTest {
+
+    private Employee myEmployee;
+
+    @BeforeEach
+    void init(){
+        myEmployee = new Employee("Conor","08576791","12345",23);
+    }
+
+    @Test
+    void testConstuctor(){
+        assertEquals("Conor",myEmployee.getName());
+        assertEquals("08576791",myEmployee.getPhone());
+        assertEquals("12345",myEmployee.getEmployeeId());
+        assertEquals(23,myEmployee.getAge());
+
+    }
+
+    @Test
+      void nameFail(){
+        Exception e = assertThrows(IllegalArgumentException.class,()->myEmployee = new Employee(" ","08576791","12345",23));
+            assertEquals("Invalid Name",e.getMessage());//invalid Name entry
+    }
+
+    @Test
+    void phoneFail(){
+        Exception e = assertThrows(IllegalArgumentException.class,()->myEmployee = new Employee("Conor"," ","12345",23));
+        assertEquals("Invalid Phone",e.getMessage());//invalid Phone entry
+    }
+
+    @Test
+    void employeeIdFail(){
+        Exception e = assertThrows(IllegalArgumentException.class,()->myEmployee = new Employee("Conor","08576791"," ",23));
+        assertEquals("Invalid ID",e.getMessage());//invalid Id entry
+    }
+
+    @Test
+    void ageFail(){
+        Exception e = assertThrows(IllegalArgumentException.class,()->myEmployee = new Employee("Conor","08576791","12345",10));
+        assertEquals("Invalid Age",e.getMessage());//invalid Age entry
+    }
+
 }
